a1316e2863bd876a531ffcdd1a451719e41b2c14
Proloycb <proloychacroborty@gmail.com>
2022-08-25 16:46:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        organictheme: {
+          primary: "#00d084",
+          secondary: "#7bdcb5",
+          accent: "#ff6900",
+          neutral: "#fcb900",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
